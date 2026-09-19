3ebc5547515f725862d11de3e2417f4a98f3d1be
Yung <lixiangyang1994@gmail.com>
2026-09-20 00:36:11 +0800

    chore: bump CloudSubscribe to 1.5.2

--- plugins.v2/cloudsubscribe/__init__.py
+++ plugins.v2/cloudsubscribe/__init__.py
@@ -102,7 +102,7 @@ class CloudSubscribe(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/odomu/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.5.1"
+    plugin_version = "1.5.2"
     # 插件作者
     plugin_author = "odomu"
     # 作者主页
